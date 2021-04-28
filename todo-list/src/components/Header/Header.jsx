b57a27e4164ff2a06db699ebb1d74ca9a59f4867
Jorge Nicolas Martin <nicomartin854@gmail.com>
2021-04-28 00:42:39 -0300

    Simplificando Header.jsx

--- todo-list/src/components/Header/Header.jsx
+++ todo-list/src/components/Header/Header.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 
-export class Header extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(props) {
-    super(props);
-  }
-  render() {
-    return (
-      <header>
-        <h1>TodoList</h1>
-      </header>
-    );
-  }
-}
+export const Header = () => (
+  <header>
+    <h1>TodoList</h1>
+  </header>
+);
